136f37c1b27695c128e46e36a4297c71bde50e51
bjiang <bjiang@pharmamedica.com>
2018-10-25 15:20:52 -0400

    Added Console.ReadLine(), so that the console only closes after you hit enter key

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,6 +7,9 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //to prevent the console window from auto-closing
+            Console.ReadLine();
         }
     }
 }
